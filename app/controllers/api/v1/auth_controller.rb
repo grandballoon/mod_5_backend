1907234c0e0dfc1adc4a310096158c2f68b0b635
grandballoon <luke.sovern.conley@gmail.com>
2018-11-03 18:23:37 -0400

    added logging for user login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,12 +3,16 @@ class Api::V1::AuthController < ApplicationController
 
   def create
     @user = User.find_by(username: user_login_params[:username])
+    puts "user is #{@user}"
+    puts "Is the user authenticated? #{@user.authenticate(user_login_params[:password])}"
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token(user_id: @user.id)
+      puts "token is #{token}"
       @user.token = token
       @user.save
       render json: {user: UserSerializer.new(@user), jwt:token}, status: :accepted
     else
+      puts "user was invalid"
       render json: {message: 'invalid username or password'}, status: :unauthorized
     end
   end
