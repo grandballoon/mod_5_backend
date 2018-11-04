27e8236e8e809e12923a824c1dbf597e433291b0
grandballoon <luke.sovern.conley@gmail.com>
2018-11-03 20:29:22 -0400

    deleted puts

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,14 +6,11 @@ class Api::V1::AuthController < ApplicationController
     puts "user is #{@user}"
     puts "the user's username is #{user_login_params[:username]}
     if @user && @user.authenticate(user_login_params[:password])
-      puts('inside authentication case')
       token = encode_token(user_id: @user.id)
-      puts(token)
       @user.token = token
       @user.save
       render json: {user: UserSerializer.new(@user), jwt:token}, status: :accepted
     else
-      puts('inside inauthentic case')
       render json: {message: 'invalid username or password'}, status: :unauthorized
     end
   end
